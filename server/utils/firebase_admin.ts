7194f8e3f7ffeae9b9bad3dc83167dfa2927c237
Saif Ayman Fathi Mohamed Metwally <65094581+metwally-saif@users.noreply.github.com>
2024-10-28 14:03:47 +0200

    Refactor firebase_admin.ts to import serviceAccount credentials

--- server/utils/firebase_admin.ts
+++ server/utils/firebase_admin.ts
@@ -1,5 +1,5 @@
 import admin from "firebase-admin";
-
+// serviceAccount is a JSON object that contains the private key and other credentials for the Firebase Admin SDK
 import serviceAccount from "./budgeting-platform-firebase-adminsdk-4hs0r-c2216b18f1.json";
 
 admin.initializeApp({
