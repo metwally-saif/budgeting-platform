8e9726564dd76208929b72c1c0af001fdddadfd1
Saif Ayman Fathi Mohamed Metwally <65094581+metwally-saif@users.noreply.github.com>
2024-12-07 18:34:52 +0200

    Add .yarnrc.yml configuration and update project version to 1.0.0

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,11 @@
 import { Route,  createRoutesFromElements, createBrowserRouter, RouterProvider} from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import LoginPage from "./pages/LoginPage";
+import { Amplify } from 'aws-amplify';
+import outputs from "../amplify_outputs.json";
+
+
+Amplify.configure(outputs);
 
 const router = createBrowserRouter(
   createRoutesFromElements(
